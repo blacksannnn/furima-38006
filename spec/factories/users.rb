ebbe1657b3d72b5aa28d4490a44147b8424e73a9
blacksannnn <platinum.black.96@gmail.com>
2022-06-05 21:13:33 +0900

    bundle exec rubocop -a後

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
-    family_name {"斉藤"}
-    first_name {"太朗"}
-    family_name_kana {"サイトウ"}
-    first_name_kana {"タロウ"}
-    birth_day {"2022-06-05"}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
+    family_name { '斉藤' }
+    first_name { '太朗' }
+    family_name_kana { 'サイトウ' }
+    first_name_kana { 'タロウ' }
+    birth_day { '2022-06-05' }
   end
 end
