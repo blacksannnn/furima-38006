8d8764cc07d02264581e817414790ce19bd3f47c
blacksannnn <platinum.black.96@gmail.com>
2022-06-05 14:05:34 +0900

    確認用

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :bfamily_name, presence: true
+  validates :family_name, presence: true
   validates :first_name, presence: true    
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
