f1af9d5fabc11ca1564879e692b7583320591663
blacksannnn <platinum.black.96@gmail.com>
2022-06-05 20:54:07 +0900

    テストコード記述

--- README.md
+++ README.md
@@ -30,13 +30,13 @@ Things you may want to cover:
 | Column                | Type   | Options     |
 | ------------------    | ------ | ----------- |
 | nickname              | string | null: false |
-| email                 | string | null: false |
+| email                 | string | null: false,unique: true |
 | encrypted_password    | string | null: false |
 | family_name           | string | null: false |
 | first_name            | string | null: false |
 | family_name_kana      | string | null: false |
 | first_name_kana       | string | null: false |
-| birth_day             | string | null: false |
+| birth_day             | date   | null: false |
 
 ### Association
 has_many :products
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true    
-  validates :family_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_day, presence: true
+  validates :nickname,         presence: true
+  validates :family_name,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :birth_day,        presence: true
 
 end
