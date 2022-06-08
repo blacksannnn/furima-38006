aff1f03648118779e3e76423e40e72275690f964
blacksannnn <platinum.black.96@gmail.com>
2022-06-08 21:06:53 +0900

    レビュー後の修正

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -89,6 +89,12 @@ RSpec.describe Product, type: :model do
         @product.valid?
         expect(@product.errors.full_messages).to include('Price is not a number')
       end
+
+      it 'userが紐付いていないと保存できない' do
+        @product.user = nil
+        @product.valid?
+        expect(@product.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
