3bd2b7c95937dd82847c3fe58fa15c8d4baf9856
blacksannnn <platinum.black.96@gmail.com>
2022-06-09 18:18:57 +0900

    レビュー後の修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @products = Product.includes(:user)
+    @products = Product.includes(:user).order("created_at ABC")
   end
 
   def new
