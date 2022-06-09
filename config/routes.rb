e0cd9d51c9eee5c11506d67ab8f3b5220ddad282
blacksannnn <platinum.black.96@gmail.com>
2022-06-09 17:55:35 +0900

    商品一覧表示機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+<<<<<<< Updated upstream
   root to: "items#index"
   resources :items, only: [:new]
+=======
+  root to: "products#index"
+  resources :products, only: [:new,:create,:index]
+   
+>>>>>>> Stashed changes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
