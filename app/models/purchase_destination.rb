ed0bcd6d93e6a7b10aa2b5f8d554c6fef2e29f9a
blacksannnn <platinum.black.96@gmail.com>
2022-06-14 22:06:39 +0900

    購入ボタン押下後DB保存可

--- app/models/purchase_destination.rb
+++ app/models/purchase_destination.rb
@@ -3,18 +3,18 @@ class PurchaseDestination
   attr_accessor  :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :token, :product_id
 
   
-  #validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" }
-  #validates :city,              presence: true
-  #validates :address,           presence: true
+  validates :prefecture_id,     numericality: { other_than: 1, message: "can't be blank" }
+  validates :city,              presence: true
+  validates :address,           presence: true
       
   
-  #VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
-  #validates :post_code,         format: { with: VALID_POSTAL_CODE_REGEX }
+  VALID_POSTAL_CODE_REGEX = /\A\d{3}[-]?\d{4}\z/
+  validates :post_code,         format: { with: VALID_POSTAL_CODE_REGEX }
 
-  #VALID_PHONE_NUMBER_REGEX = /\A\d{10,11}\z/
-  #validates :phone_number,      format: { with: VALID_PHONE_NUMBER_REGEX }
+  VALID_PHONE_NUMBER_REGEX = /\A\d{10,11}\z/
+  validates :phone_number,      format: { with: VALID_PHONE_NUMBER_REGEX }
 
-  #validates :token, presence: true
+  validates :token, presence: true
 
   def save
     purchase = Purchase.create( user_id:  user_id, product_id: product_id)
